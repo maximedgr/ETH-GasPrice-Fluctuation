aeace7ddb492874a55c0b163c925439dcff39d1e
Maxime <sowlution.pro@gmail.com>
2022-11-23 18:09:22 +0100

    rush

--- README.md
+++ README.md
@@ -3,3 +3,7 @@ Application alerting a user via Telegram message and web visualization of abnorm
 
 # Etherscan API :
 [Gas Price API](https://api.etherscan.io/api?module=gastracker&action=gasoracle)
+
+create database OS_Project;
+use OS_Project;
+create table GasPrice(USD_price VARCHAR(30) NOT NULL, date VARCHAR(30) NOT NULL, PRIMARY KEY (date));
--- bot.sh
+++ bot.sh
@@ -2,3 +2,11 @@
 curl "https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=JCSHH896K1VMHXII4BITGYEDWUJ7W64AMZ" > api_data.txt
 echo "Bot is running..."
 echo $(cat api_data.txt | grep -oP '(?<="SafeGasPrice":").*?(?=")')
+
+# create database OS_Project;
+# use OS_Project;
+# create table GasPrice(USD_price VARCHAR(30) NOT NULL, date VARCHAR(30) NOT NULL, PRIMARY KEY (date));
+
+sudo mysql
+USE OS_Project;
+INSERT INTO GasPrice(USD_price,date) VALUES("16","23/11/2022");
