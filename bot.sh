58da076df215bfb1faa3e599d225e772ee8164eb
Maxime <sowlution.pro@gmail.com>
2022-11-23 18:26:54 +0100

    zebi

--- bot.sh
+++ bot.sh
@@ -3,12 +3,5 @@ curl "https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=JCS
 echo "Bot is running..."
 echo $(cat api_data.txt | grep -oP '(?<="SafeGasPrice":").*?(?=")')
 
-# create database OS_Project;
-# use OS_Project;
-# create table GasPrice(USD_price VARCHAR(30) NOT NULL, date VARCHAR(30) NOT NULL, PRIMARY KEY (date));
-
-
-
 
 sqlite3 test.db  "create table GasPrice(USD_price VARCHAR(30) NOT NULL, date VARCHAR(30) NOT NULL, PRIMARY KEY (date));"
-sqlite3 test.db  "INSERT INTO GasPrice(USD_price,date) VALUES("17","24/11/2022:01");"
---
+++ insert.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sqlite3 test.db  "INSERT INTO GasPrice(USD_price,date) VALUES('17','24/11/2022');"
