058094c7973e3402d74d7df5d74186e4e5208313
Maxime <sowlution.pro@gmail.com>
2022-11-23 18:24:01 +0100

    c

--- bot.sh
+++ bot.sh
@@ -7,17 +7,8 @@ echo $(cat api_data.txt | grep -oP '(?<="SafeGasPrice":").*?(?=")')
 # use OS_Project;
 # create table GasPrice(USD_price VARCHAR(30) NOT NULL, date VARCHAR(30) NOT NULL, PRIMARY KEY (date));
 
-sudo mysql
-USE OS_Project;
-INSERT INTO GasPrice(USD_price,date) VALUES("17","24/11/2022:01");
-EOF
 
 
-sudo mysql test.db <<EOF
-create table GasPrice(USD_price VARCHAR(30) NOT NULL, date VARCHAR(30) NOT NULL, PRIMARY KEY (date));
-INSERT INTO GasPrice(USD_price,date) VALUES("17","24/11/2022:01");
-select * from GasPrice;
-EOF
 
 sqlite3 test.db  "create table GasPrice(USD_price VARCHAR(30) NOT NULL, date VARCHAR(30) NOT NULL, PRIMARY KEY (date));"
 sqlite3 test.db  "INSERT INTO GasPrice(USD_price,date) VALUES("17","24/11/2022:01");"
