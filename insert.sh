175405919d6ffbba26be7eab82ab36a89256a98f
Maxime <sowlution.pro@gmail.com>
2022-11-27 22:50:41 +0100

    it works

--- insert.sh
+++ insert.sh
@@ -3,7 +3,7 @@ gasPrice=$(cat api_data.txt | grep -oP '(?<="SafeGasPrice":").*?(?=")')
 lastBlock=$(cat api_data.txt | grep -oP '(?<="LastBlock":").*?(?=")')
 date_=$(date '+%Y-%m-%d %H:%M:%S')
 
-sqlite3 gas_tab.db  "INSERT INTO GasPrice(USD_price,date,blocktime) VALUES($gasPrice,$date_,$lastBlock);"
+sqlite3 gas_tab.db  "INSERT INTO GasPrice(USD_price,date,blocktime) VALUES($gasPrice,'$date_',$lastBlock);"
 
 echo "Done -> INSERT INTO GasPrice(USD_price,date,blocktime) VALUES($gasPrice,$date_,$lastBlock);"
 
