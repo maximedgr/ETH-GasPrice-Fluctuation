6ea567d0cfbf940a635686c8f1028c791f17a975
Maxime <sowlution.pro@gmail.com>
2022-11-27 20:41:42 +0100

    up

--- insert.sh
+++ insert.sh
@@ -3,8 +3,7 @@ gasPrice= echo $(cat api_data.txt | grep -oP '(?<="SafeGasPrice":").*?(?=")')
 lastBlock= echo $(cat api_data.txt | grep -oP '(?<="LastBlock":").*?(?=")')
 date_= echo $(date +"%D %T")
 
-#sqlite3 gas_tab.db  "INSERT INTO GasPrice(USD_price,date) VALUES($GasPrice,$Date_,$LastBlock);"
+sqlite3 gas_tab.db  "INSERT INTO GasPrice(USD_price,date) VALUES($GasPrice,$Date_,$LastBlock);"
 
-echo $gasPrice
 
 
