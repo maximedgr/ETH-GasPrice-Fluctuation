e11689bc703384ef3976d2bc6636affb5f344f7b
Maxime <sowlution.pro@gmail.com>
2022-11-27 22:22:56 +0100

    fix it ?

--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sqlite3 must be installed
-sqlite3 gas_tab.db  "create table GasPrice(USD_price VARCHAR(50) NOT NULL, date DATETIME NOT NULL, blocktime VARCHAR(50) NOT NULL, PRIMARY KEY (blocktime));"
+sqlite3 gas_tab.db  "create table GasPrice(USD_price VARCHAR(50) NOT NULL, date TEXT NOT NULL, blocktime VARCHAR(50) NOT NULL, PRIMARY KEY (blocktime));"
