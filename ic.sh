3de1f5581c597bd29b24fe1f357739171c6e2b75
Maxime <sowlution.pro@gmail.com>
2022-11-29 00:08:00 +0100

    not really fix

--- ic.sh
+++ ic.sh
@@ -28,10 +28,12 @@ last_ano_value=$(sqlite3 gas_tab.db "SELECT Anomalie.USD_price FROM Anomalie WHE
 #On store juste le blocktime de la dernière anomalie et on compare si c'est la même ou une nouvelle de détectée
 if [[ $last_ano == "" ]]; #Si fichier vide
 then
+echo "No previous anomalie"
 echo $outsidevalueTime >> last_anomalie.txt
 else
 if [[ $last_ano_value != $outsidevalue &&  $outsidevalue != "" ]];
 then
+echo "last_ano_value : "$last_ano_value" outsidevalue : "$outsidevalue
 echo "last_ano : "$last_ano" outsidevalueTime : "$outsidevalueTime
 echo "Anomalie detected : "$outsidevalue" | Blocktime : "$outsidevalueTime " | Date : "$outsidevalueDate
 sqlite3 gas_tab.db  "INSERT INTO Anomalie(USD_price,date,blocktime) VALUES($outsidevalue,'$outsidevalueDate',$outsidevalueTime);"
